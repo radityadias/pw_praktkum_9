3ae5d53175ce2833789107274171980cda13b481
radityadias <radityadias24@gmail.com>
2024-06-06 17:05:36 +0700

    Ntah masih bingung

---
+++ company-profile-frontend/src/component/Home.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>Welcome to PT GIT Solution</h1>
+        <p>Solusi Teknologi Terbaik untuk Bisnis Anda</p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    name: 'Home',
+    data() {
+        return {
+            profile: {}
+        };
+    },
+    created() {
+        axios.get('http://localhost:3000/profile')
+            .then(response => {
+                this.profile = response.data;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
+};
+</script>
